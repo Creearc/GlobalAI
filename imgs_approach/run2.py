e0b7be5c0453ae32925872a1741ded58a4858e5a
Alexandr <knxandr@rambler.ru>
2022-02-15 23:04:13 +0500

    Update run2.py

--- imgs_approach/run2.py
+++ imgs_approach/run2.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import f1_score, accuracy_score
 import cv2
+import time
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(gpus[0], True)
