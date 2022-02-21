1a288501f8bfd47f00f1541bfd5e163aa544096c
Alexandr <knxandr@rambler.ru>
2022-02-21 21:49:44 +0500

    Update train.py

--- train.py
+++ train.py
@@ -23,6 +23,38 @@ def smi_to_descriptors(smile):
         descriptors = np.array(discr_calc.CalcDescriptors(mol))
     return descriptors
 
+
+def input_fn(features, labels, shuffle, num_epochs, batch_size):
+  """Generates an input function to be used for model training.
+
+  Args:
+    features: numpy array of features used for training or inference
+    labels: numpy array of labels for each example
+    shuffle: boolean for whether to shuffle the data or not (set True for
+      training, False for evaluation)
+    num_epochs: number of epochs to provide the data for
+    batch_size: batch size for training
+
+  Returns:
+    A tf.data.Dataset that can provide data to the Keras model for training or
+      evaluation
+  """
+  if labels is None:
+    inputs = features
+  else:
+    inputs = (features, labels)
+  dataset = tf.data.Dataset.from_tensor_slices(inputs)
+
+  if shuffle:
+    dataset = dataset.shuffle(buffer_size=len(features))
+
+  # We call repeat after shuffling, rather than before, to prevent separate
+  # epochs from blending together.
+  dataset = dataset.repeat(num_epochs)
+  dataset = dataset.batch(batch_size)
+  return dataset
+
+
 for elem in df['Smiles']:
     features.append(smi_to_descriptors(elem))
 
@@ -40,14 +72,22 @@ model = tf.keras.Model(input_layer, conc)
 
 model.summary()
 
+BATCH_SIZE = 32
+
 EPOCHS = 10
 LR = 1e-5
 model.compile(optimizer=tf.keras.optimizers.Adam(LR),
                       loss=tf.keras.losses.CategoricalCrossentropy(label_smoothing=0.1),
                       metrics=['accuracy'])
 
-model.fit(X,
-          steps_per_epoch=len(X),
+training_dataset = input_fn(features=X.values,
+                    labels=Y,
+                    shuffle=True,
+                    num_epochs=EPOCHS,
+                    batch_size=BATCH_SIZE)
+
+model.fit(training_dataset,
+          steps_per_epoch=len(training_dataset),
           epochs=EPOCHS,
-          validation_data=X,
-          validation_steps=len(X))
+          validation_data=training_dataset,
+          validation_steps=len(training_dataset))
